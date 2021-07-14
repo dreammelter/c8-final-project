9ccf4ea4f5d13aab29279dc8e45918241d96794c
dreammelter <dreammelter@users.noreply.github.com>
2021-07-14 16:00:27 -0700

    Clean up model comments and init migration

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -97,13 +97,9 @@ class Enrollment(models.Model):
 
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
-    # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
-    # Has a grade point for each question
-    # Has question content
-    # Other fields and methods you would like to design
 class Question(models.Model):
     # Foreign key to lesson
-    lesson = model.ForeignKey(Lesson, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     #One-to-Many Course Relationship?
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
@@ -124,10 +120,6 @@ class Question(models.Model):
 
 #  <HINT> Create a Choice Model with:
     # Used to persist choice content for a question
-    # One-To-Many (or Many-To-Many if you want to reuse choices) relationship with Question
-    # Choice content
-    # Indicate if this choice of the question is a correct one or not
-    # Other fields and methods you would like to design
 class Choice(models.Model):
     # Many-To-Many to make choices reusable cuz why not
     questions = models.ManyToManyField(Question)
@@ -137,9 +129,7 @@ class Choice(models.Model):
     is_correct = models.BooleanField()
 
 # <HINT> The submission model
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
+# One choice could belong to multiple submissions (...backwards access?)
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     chocies = models.ManyToManyField(Choice)
